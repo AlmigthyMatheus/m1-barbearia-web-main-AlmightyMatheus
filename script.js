2edd59d2d68583a7d11c7cc03906168d8f004d82
AlmightyMatheus <matheusbrandaomatheus@gmail.com>
2024-05-24 20:15:55 -0300

    m1-barbearia-web-AlmightyMatheus

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const barbearia = {
         { id: 4, tipo: "Moicano", valor: 10 },
         { id: 5, tipo: "Razor part", valor: 5 },
     ],
-  
+
     barbas: [
         { id: 1, tipo: "Capitão Jack", valor: 20 },
         { id: 2, tipo: "Van Dyke", valor: 20 },
@@ -16,30 +16,65 @@ const barbearia = {
     ],
     estaAberto: true,
 };
-  
-  
+
+
 function buscaCortePorId(id) {
+    for (let i = 0; i < barbearia.cortes.length; i++) {
+        if (barbearia.cortes[i].id === id) {
+            return barbearia.cortes[i];
+        }
+    }
+    return "Corte não encontrado";
 }
 
 function buscaBarbaPorId(id) {
+    for (let i = 0; i < barbearia.barbas.length; i++) {
+        if (barbearia.barbas[i].id === id) {
+            return barbearia.barbas[i];
+        }
+    }
+    return "Barba não encontrada";
 }
 
 function verificaStatusBarbearia() {
+    if (barbearia.estaAberto === true) {
+        return "Estamos abertos";
+    }
+    return "Estamos fechados";
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes;
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas;
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    let corte = buscaCortePorId(corteId);
+    let barba = buscaBarbaPorId(barbaId);
+
+    let pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor
+    };
+    return pedido;
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].id === id) {
+            lista[i] = { id: id, tipo: tipo, valor: valor };
+            return lista;
+        }
+    }
+    return lista;
 }
 
 function calculaTotal(pedido) {
+    return pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
 }
-  
-  
